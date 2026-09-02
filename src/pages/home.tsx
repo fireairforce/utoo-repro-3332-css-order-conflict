0e9f238afdbe3eca6bbeb437bb2657dc0cb8e96d
zoomdong <1344492820@qq.com>
2026-09-02 17:16:54 +0800

    chore: add test case

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,10 @@
 import "../styles/reset.css";
 import "../styles/theme.css";
+import { ACCESS } from './type2s';
+
+const access: ACCESS = 'access';
+
+access;
 
 export default function HomePage() {
   return <button className="button">Home</button>;
